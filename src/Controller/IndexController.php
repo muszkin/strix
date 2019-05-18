133067fcfd94b878a2e8ca7c2c74164a381f1029
Muszkin <muszkin@gmail.com>
2019-05-18 11:08:35 +0200

    add phpunit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController
+{
+    /**
+     * @return array
+     * @Route("/",name="index")
+     * @Template("Index/index.html.twig")
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+}
---
+++ tests/Controller/IndexControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class IndexControllerTest extends WebTestCase
+{
+    public function testIndexAction()
+    {
+        $client = static::createClient();
+        $crawler = $client->request("GET","/");
+
+        $this->assertEquals(200,$client->getResponse()->getStatusCode());
+
+        $this->assertGreaterThan(0,$crawler->filter("h1:contains('Hello world')")->count());
+    }
+}
